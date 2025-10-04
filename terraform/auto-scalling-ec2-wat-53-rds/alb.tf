d144e593dbf65bc2dd0f49bccf55cd59a5937b2c
Felipe Salvador <f.salvador91@gmail.com>
2025-10-04 16:07:01 -0300

    - github action provision - rename folder

--- terraform/auto-scalling-ec2-wat-53-rds/alb.tf
+++ terraform/auto-scalling-ec2-wat-53-rds/alb.tf
@@ -20,16 +20,6 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-# resource "aws_lb_listener" "http" {
-#   load_balancer_arn = aws_lb.app_alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
-#   default_action {
-#     type = "forward"
-#     target_group_arn = aws_lb_target_group.app_tg.arn
-#   }
-# }
-
 resource "aws_lb_target_group" "app_tg" {
   name     = "app-tg"
   port     = 80
