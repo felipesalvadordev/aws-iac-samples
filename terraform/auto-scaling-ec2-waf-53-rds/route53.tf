3a7642c09ab155aca3a2500dff55128bb61213c8
Felipe Salvador <f.salvador91@gmail.com>
2026-01-31 10:22:30 -0300

    Update route53.tf

--- terraform/auto-scaling-ec2-waf-53-rds/route53.tf
+++ terraform/auto-scaling-ec2-waf-53-rds/route53.tf
@@ -1,16 +1,17 @@
-data "aws_route53_zone" "main" {
-  name       = "app.salvador.com." # Ensure the domain name ends with a dot
-  depends_on = [aws_route53_zone.main]
-}
-
 resource "aws_route53_zone" "main" {
   name = "app.salvador.com"
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www"
   type    = "A"
+  
+  depends_on = [aws_route53_zone.main]
+
   alias {
     name                   = aws_lb.app_alb.dns_name
     zone_id                = aws_lb.app_alb.zone_id
