d9b393222c273c767baa60615f2b89b0b0e5d160
Felipe Salvador <f.salvador91@gmail.com>
2026-01-31 09:17:51 -0300

    removing nat-gateway config

--- terraform/auto-scaling-ec2-waf-53-rds/vpc.tf
+++ terraform/auto-scaling-ec2-waf-53-rds/vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = { Name = "main-vpc" }
 }
@@ -59,19 +59,8 @@ resource "aws_route_table_association" "public_b" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_eip" "nat" {}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_a.id
-  tags = { Name = "main-nat" }
-}
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
   tags = { Name = "private-rt" }
 }
 
